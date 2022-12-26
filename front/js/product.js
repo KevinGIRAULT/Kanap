e190d3b632879f5995d4d72ce003d4f959d6283c
KevinGIRAULT <kevgiraultdev@gmail.com>
2022-12-26 17:55:14 -0300

    improvement of the readability of the code

--- front/js/product.js
+++ front/js/product.js
@@ -4,30 +4,29 @@ fetch("http://localhost:3000/api/products")
             return result.json();
         }
     })
-    .then((product) => {
+    .then((arrayOfProducts) => {
         const searchParams = new URLSearchParams(window.location.search);
-        console.log("searchParams vaut : " + searchParams);
         const idProduct = searchParams.get("id");
-        console.log("idproduct vaut " + idProduct);
 
-        const objectProduct = product.findIndex(function (element) {
-            // Dans findIndex (et find), il y a un if implicite. Il teste si chaque element._id vaut idProduct et quand c'est le cas, il le dit à findIndex. Alors, findIndex fait un return DE L'INDEX de l'élément dans le tableau (et non pas un true or false comme on pourrait le croire)
+        const objectProduct = arrayOfProducts.findIndex(function (element) {
             return element._id === idProduct;
         });
 
         if (idProduct === searchParams.get("id")) {
+
             document.querySelector(
                 ".item__img"
-            ).innerHTML = `<img src="${product[objectProduct].imageUrl}" alt="${product[objectProduct].altTxt}">`;
-
+            ).innerHTML = `<img src="${arrayOfProducts[objectProduct].imageUrl}" alt="${arrayOfProducts[objectProduct].altTxt}">`;
             document.getElementById("title").textContent =
-                product[objectProduct].name;
+                arrayOfProducts[objectProduct].name;
             document.getElementById("description").textContent =
-                product[objectProduct].description;
+                arrayOfProducts[objectProduct].description;
             document.getElementById("price").textContent =
-                product[objectProduct].price;
+                arrayOfProducts[objectProduct].price;
 
-            const variable = product[objectProduct].colors.forEach((color) => {
+            const colorOptionsString = arrayOfProducts[
+                objectProduct
+            ].colors.forEach((color) => {
                 let colour = "";
                 colour = `<option value="${color}">${color}</option>`;
                 const selectElement = (document.querySelector(
@@ -35,4 +34,7 @@ fetch("http://localhost:3000/api/products")
                 ).innerHTML += colour);
             });
         }
+    })
+    .catch(() => {
+        console.log(error);
     });
