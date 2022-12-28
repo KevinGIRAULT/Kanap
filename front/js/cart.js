714f9ff745fa3dd2118828b9165333ca37a4b68f
KevinGIRAULT <kevgiraultdev@gmail.com>
2022-12-28 18:33:44 -0300

    create object, put it in array et enter it into localStorage

---
+++ front/js/cart.js
@@ -0,0 +1,11 @@
+/* À faire :
+Quand je clique sur le bouton "Ajouter au panier", un tableau situé dans cart.js se remplie des informations présentes sur la page
+
+Pour cela, par étape :
+- remplir dans le localStorage les informations ajoutées via le bouton "ajouter au panier" présent sur la page product.html
+- les récupérer ici depuis le localstorage et les mettres dans un objet
+*/
+
+const cart = [
+
+]
--- front/js/product.js
+++ front/js/product.js
@@ -13,7 +13,6 @@ fetch("http://localhost:3000/api/products")
         });
 
         if (idProduct === searchParams.get("id")) {
-
             document.querySelector(
                 ".item__img"
             ).innerHTML = `<img src="${arrayOfProducts[objectProduct].imageUrl}" alt="${arrayOfProducts[objectProduct].altTxt}">`;
@@ -38,3 +37,41 @@ fetch("http://localhost:3000/api/products")
     .catch(() => {
         console.log(error);
     });
+
+let cart = [
+    {
+        id: "",
+        quantity: "",
+        color: "",
+    },
+];
+
+function putInArray() {
+    document.getElementById("quantity").addEventListener("input", (event) => {
+        cart.quantity = event.target.value;
+    });
+
+    document.getElementById("colors").addEventListener("input", (event) => {
+        cart.color = event.target.value;
+    });
+
+    cart.id = new URLSearchParams(window.location.search).get("id");
+}
+
+putInArray();
+
+document.getElementById("addToCart").addEventListener("click", () => {
+    const regexOnlyNumberBetween0AND100 = /^(?:100|[1-9][0-9]?)$/;
+
+    if (
+        regexOnlyNumberBetween0AND100.test(cart.quantity) &&
+        cart.color !== undefined &&
+        cart.color !== ""
+    ) {
+        const cartInString = JSON.stringify(cart);
+        localStorage.setItem("autonoé", cartInString);
+        alert("l'ajout au panier est bien pris en compte");
+    } else {
+        alert("Faites vos choix");
+    }
+});
