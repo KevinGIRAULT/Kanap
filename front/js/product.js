f5636ec7bd522d5114ad824a840f39931b025ff8
KevinGIRAULT <kevgiraultdev@gmail.com>
2023-01-04 05:34:03 -0300

    Checking if a product is already in the shopping cart only works if it is. Next step: solve the problem that arises when adding a different product.

--- front/js/product.js
+++ front/js/product.js
@@ -37,48 +37,84 @@ fetch("http://localhost:3000/api/products/" + idProduct)
         console.log(error);
     });
 
-let cart = {
-    id: "",
-    quantity: "",
-    color: "",
-};
+let cart = [];
 
 function putInArray() {
+    let obj = {
+        id: "",
+        quantity: "",
+        color: "",
+    };
     document.getElementById("quantity").addEventListener("input", (event) => {
-        cart.quantity = event.target.value;
+        obj.quantity = event.target.value;
+        console.log(obj.quantity);
+        
     });
 
     document.getElementById("colors").addEventListener("input", (event) => {
-        cart.color = event.target.value;
+        obj.color = event.target.value;
     });
 
-    cart.id = new URLSearchParams(window.location.search).get("id");
-    console.log(cart.id);
+    obj.id = new URLSearchParams(window.location.search).get("id");
+
+    cart.push(obj);
 }
 
 putInArray();
 
-function checkIfProductAlreadyInCart() {
-    if (localStorage.getItem(nameOfProduct + "_" + cart.color) !== null) {
-        let productFromLocalStorage = localStorage.getItem(nameOfProduct + "_" + cart.color);
-        let toJSON = JSON.parse(productFromLocalStorage);
+// function checkIfProductAlreadyInCart() {
+//     if () {
+//         // si le local storage est différent de nul
 
-        if (toJSON.id === cart.id && toJSON.color === cart.color) {
-            cart.quantity = +cart.quantity + +toJSON.quantity;
+//     }
+
+//     for (let i = 0; i < cart.length; i++) {
+//         if (cart[i].nameOfProduct === nameOfProduct && cart[i].color === cart.color) {
+//             cart[i].quantity = +cart[i].quantity + +cart.quantity;
+//             // return;
+//         }
+//     }
+//     cart.push({nameOfProduct: nameOfProduct, color: cart.color, quantity: cart.quantity});
+// }
+
+function checkIfProductAlreadyInCart() {
+
+    if (localStorage.getItem("cart") !== null) {
+        let stringCart = localStorage.getItem("cart");
+        let arrayCart = JSON.parse(stringCart);
+
+
+        const index = arrayCart.findIndex(function(item) {
+            return item.color === cart[0].color && item.id === cart[0].id;
+          });
+
+        // SI la couleur et l'ID de ce qu'on ajoute au panier correspondent à ceux déjà présents dans le panier
+        if (cart[0].color === arrayCart[index].color && cart[0].id === arrayCart[index].id ) {
+            cart[0].quantity = +cart[0].quantity + +arrayCart[index].quantity;
         }
+        // Alors incrémenter la quantité
+          
+        // let productFromLocalStorage = localStorage.getItem(nameOfProduct + "_" + cart.color);
+        // let toJSON = JSON.parse(productFromLocalStorage);
+
+        // if (toJSON.id === cart.id && toJSON.color === cart.color) {
+        //     cart.quantity = +cart.quantity + +toJSON.quantity;
+        // }
     }
 }
 
+
 document.getElementById("addToCart").addEventListener("click", () => {
     if (
-        cart.quantity <= 100 &&
-        cart.quantity > 0 &&
-        cart.color !== undefined &&
-        cart.color !== ""
+        cart[0].quantity <= 100 &&
+        cart[0].quantity > 0 &&
+        cart[0].color !== undefined &&
+        cart[0].color !== ""
     ) {
         checkIfProductAlreadyInCart();
         const cartInString = JSON.stringify(cart);
-        localStorage.setItem(nameOfProduct + "_" + cart.color, cartInString);
+        console.log(cartInString);
+        localStorage.setItem("cart", cartInString);
         alert("l'ajout au panier est bien pris en compte");
     } else {
         alert("Faites vos choix");
