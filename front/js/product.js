28ed66f39b4e3dd763b047e76d3748125f45b827
KevinGIRAULT <kevgiraultdev@gmail.com>
2023-01-10 12:02:09 -0300

    add product  when localStorage empty : good. Thereafter, you will have to manage the addition of a product already present

--- front/js/product.js
+++ front/js/product.js
@@ -30,81 +30,9 @@ fetch("http://localhost:3000/api/products/" + idProduct)
     .catch(() => {
         console.log(error);
     });
-/**/
-
-// let cart = [];
-// let obj;
-
-// function putInArray() {
-//     obj = {
-//         id: "",
-//         quantity: "",
-//         color: "",
-//     };
-//     document.getElementById("quantity").addEventListener("input", (event) => {
-//         obj.quantity = event.target.value;
-//         console.log("objet quantity : " + obj.quantity);
-//     });
-
-//     document.getElementById("colors").addEventListener("input", (event) => {
-//         obj.color = event.target.value;
-//         console.log("objet color : " + obj.color);
-//     });
-
-//     obj.id = new URLSearchParams(window.location.search).get("id");
-
-//     cart.push(obj);
-// }
-
-// putInArray();
-
-// function ifProductAlreadyInCart() {
-//     if (localStorage.getItem("cart") !== null) {
-//         const itemObtained = localStorage.getItem("cart");
-//         const itemJSON = JSON.parse(itemObtained);
-
-//         const index = itemJSON.findIndex(function (item) {
-//             return item.color === cart[0].color && item.id === cart[0].id;
-//         });
-//         console.log(index);
-
-//         //
-//         if (index !== -1) {
-//             if (
-//                 cart[0].id === itemJSON[index].id &&
-//                 cart[0].color === itemJSON[index].color
-//             ) {
-//                 cart[0].quantity =
-//                     +cart[0].quantity + +itemJSON[index].quantity;
-//             }
-//         } else {
-//             console.log("dans le else");
-//             cart = cart.concat(itemJSON);
-//         }
-//     }
-// }
-
-// document.getElementById("addToCart").addEventListener("click", () => {
-//     if (
-//         cart[0].quantity <= 100 &&
-//         cart[0].quantity > 0 &&
-//         cart[0].color !== undefined &&
-//         cart[0].color !== ""
-//     ) {
-//         ifProductAlreadyInCart();
-//         const cartInString = JSON.stringify(cart);
-//         localStorage.setItem("cart", cartInString);
-//         alert("L'ajout au panier est bien pris en compte");
-//         cart[0].quantity = 1;
-//         document.getElementById("quantity").value = 1;
-//         const selectElement = document.getElementById("colors");
-//         selectElement.selectedIndex = 0;
-//     } else {
-//         alert("Faites vos choix");
-//     }
-// });
 
 let cart = [];
+let productObject;
 
 document.getElementById("addToCart").addEventListener("click", () => {
     const quantityElement = document.getElementById("quantity").value;
@@ -118,10 +46,8 @@ document.getElementById("addToCart").addEventListener("click", () => {
 
     if (verifyLegalityUserInput(quantityElement, colorsValue)) {
         checkIfProductAlreadyInCart(productObject);
-        // cart.push(productObject);
-        console.log(cart);
     } else {
-        console.log("Faites vos choix");
+        alert("Faites vos choix");
     }
 });
 
@@ -135,16 +61,25 @@ function verifyLegalityUserInput(aQuantity, aColor) {
 }
 
 function checkIfProductAlreadyInCart(anObjectToPushInCart) {
+    console.log(localStorage.getItem("cart"));
     const cartGettedFromLocalStorage = JSON.parse(localStorage.getItem("cart"));
 
     if (cartGettedFromLocalStorage === null) {
         cart.push(anObjectToPushInCart);
+    } else {
+        productIsInCartYet(anObjectToPushInCart, cartGettedFromLocalStorage);
     }
+    cartToString = JSON.stringify(cart);
+    localStorage.setItem("cart", cartToString);
+    alert("Le produit est ajouté au panier");
 }
 
-function ProductIsInCartYet() {
-    const index = cartGettedFromLocalStorage.find((objet) => {
-        // Retourner l'objet qui a le même id et la même couleur que ce qui est ajouté
-        return
+function productIsInCartYet(anObjetToCompare, cartGettedFromLocalStorage) {
+    const oneObject = cartGettedFromLocalStorage.find((anObjectToReturn) => {
+        return (
+            anObjectToReturn.id === anObjetToCompare.id &&
+            anObjectToReturn.color === anObjetToCompare.color
+        );
     });
+    console.log(oneObject);
 }
