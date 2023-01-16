2de5254cfa90b987f7458336c6915e25e40bb20a
KevinGIRAULT <kevgiraultdev@gmail.com>
2023-01-16 09:32:00 -0300

    Insertion in DOM of the added quantity realized

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,53 +3,48 @@ const totalQuantityElement = document.getElementById("totalQuantity");
 const totalPriceElement = document.getElementById("totalPrice");
 const firstNameErrorMsgElement = document.getElementById("firstNameErrorMsg");
 
-let object = {
+let product = {
     urlOfProduct: null,
     altTexte: null,
     nameOfProduct: null,
     priceOfProduct: null,
 };
 
-// function unePromesse() {
-//     return new Promise((htmlElementsGeneration) => {
-//         if (getProduct) {
-//             console.log(getProduct);
-//             htmlElementsGeneration();
-//         }
-//     });
-// }
-
-// promise = unePromesse();
-// promise.then(htmlElementsGeneration);
-
-function getProduct(itemId /*price, alt, name, url*/) {
-    fetch("http://localhost:3000/api/products/" + itemId)
-        .then((result) => result.json())
-        .then((product) => {
-            console.log(product);
-            object.urlOfProduct = product.imageUrl;
-            object.altTexte = product.altTxt;
-            object.nameOfProduct = product.name;
-            object.priceOfProduct = product.price;
-        })
-        .catch((error) => console.log(error));
+async function getProduct(itemId) {
+    try {
+        const returnedProduct = await (
+            await fetch("http://localhost:3000/api/products/" + itemId)
+        ).json();
+        product.urlOfProduct = returnedProduct.imageUrl;
+        product.altTexte = returnedProduct.altTxt;
+        product.nameOfProduct = returnedProduct.name;
+        product.priceOfProduct = returnedProduct.price;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-function htmlElementsGeneration() {
+async function htmlElementsGeneration() {
     const itemsFromLocalStorage = JSON.parse(localStorage.getItem("cart"));
     let html = "";
-    itemsFromLocalStorage.forEach((item) => {
-        getProduct(item.id);
-        console.log(object.urlOfProduct);
+
+    for (const item of itemsFromLocalStorage) {
+        insertQuantity(itemsFromLocalStorage);
+        
+        // let additionPrice = 0;
+        // additionPrice += product.priceOfProduct
+        // totalPriceElement.textContent = additionPrice;
+
+        await getProduct(item.id);
         html += `<article class="cart__item" data-id="${item.id}" data-color="${item.color}">
         <div class="cart__item__img">
-          <img src="${object.urlOfProduct}" alt="${object.altTexte}">
+          <img src="${product.urlOfProduct}" alt="${product.altTexte}">
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__description">
-            <h2>${object.nameOfProduct}</h2>
+            <h2>${product.nameOfProduct}</h2>
             <p>${item.color}</p>
-            <p>${object.priceOfProduct},00 €</p>
+            <p>${product.priceOfProduct},00 €</p>
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
@@ -64,9 +59,15 @@ function htmlElementsGeneration() {
       </article>`;
 
         document.getElementById("cart__items").innerHTML = html;
-
-        console.log(item);
-    });
+    }
 }
 
 htmlElementsGeneration();
+
+function insertQuantity(itemsFromLocalStorage) {
+    let additionQuantity = 0;
+    itemsFromLocalStorage.forEach((element) => {
+        additionQuantity += element.quantity;
+    });
+    totalQuantityElement.textContent = additionQuantity;
+}
