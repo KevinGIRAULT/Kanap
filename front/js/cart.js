ce2bbb17e4e63126ce88402c807d4c109c7d571f
KevinGIRAULT <kevgiraultdev@gmail.com>
2023-01-31 20:33:58 -0300

    Start of management of form

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,5 @@
 const totalQuantityElement = document.getElementById("totalQuantity");
 const totalPriceElement = document.getElementById("totalPrice");
-const firstNameErrorMsgElement = document.getElementById("firstNameErrorMsg");
 let addedUpPrices = 0;
 
 let product = {
@@ -95,6 +94,7 @@ function changeQuantity() {
         );
     }
     localStorage.setItem("cart", JSON.stringify(itemsFromLocalStorage));
+
     let sum = 0;
     document.querySelectorAll(".itemQuantity").forEach((item) => {
         if (verifyInputOfQuantity(item)) {
@@ -142,22 +142,70 @@ function deleteAnItem() {
     }
 }
 
-function verifyInputOfQuantity(objet) {
+function verifyInputOfQuantity(InputObject) {
     return (
-        objet.value >= 1 &&
-        objet.value <= 100 &&
-        objet.value !== "" &&
-        objet.value !== undefined
+        InputObject.value >= 1 &&
+        InputObject.value <= 100 &&
+        InputObject.value !== "" &&
+        InputObject.value !== undefined
     );
 }
 
+function manageForm() {
+    /* This regEx follow the official standard RFC 5322. From https://emailregex.com/ (POSIX norme) and adapted for JavaScript (ECMAScript norme) */
+    const regexEmail =
+        /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+        /* Only for number et name of street, not city and postal code */
+    const regexAddressLine = /^\d+\s[A-Za-zéèîïêëàäù]+/;
+
+
+    const firstName = document.getElementById("firstName").value;
+    const firstNameErrorMsgElement = document.getElementById("firstNameErrorMsg");
+
+        const lastName = document.getElementById("lastName").value;
+        const lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+
+        const address = document.getElementById("address").value;
+        const addressErrorMsg = document.getElementById("addressErrorMsg");
+
+        const city = document.getElementById("city").value; 
+        const cityErrorMsg = document.getElementById("cityErrorMsg");
+
+
+    const email = document.getElementById("email").value;
+    const emailErrorMsg = document.getElementById("emailErrorMsg");
+
+    if (regexAddressLine.test(address)) {
+        console.log("good");
+    } else {
+        addressErrorMsg.textContent = "Le numéro et/ou le nom de la rue est/sont incorrect(s)";
+    }
+
+    if (regexEmail.test(email)) {
+        console.log("good");
+    } else {
+        emailErrorMsg.textContent = "L'adresse courriel que vous avez saisie est incorrecte";
+    }
+}
+
+manageForm();
 
 
 
+async function getCityFromAPI(city, postalCode) {
+    try {
+        const test = await (
+            await fetch("https://geo.api.gouv.fr/communes")
+        ).json();
+        console.log(test)
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-// function manageForm() {
-//     /* This regEx follow the official standard RFC 5322. From https://emailregex.com/ (POSIX norme) and adapted for JavaScript (ECMAScript norme) */
-//     const regexEmail =
-//         /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-//     console.log(regexEmail);
-// }
+getCityFromAPI();
+
+function searchCities(cities, searchText) {
+
+}
